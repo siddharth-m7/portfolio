ec5ffcd8019384e21a5fac23f1396585da757327
Siddharth Mishra <siddharthmishra2709@gmail.com>
2025-09-04 22:42:31 +0530

    Add padding to the Projects heading

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -104,7 +104,7 @@ export default function Projects() {
         <section className="text-white w-full pt-20 bg-slate-950">
           <div className="flex flex-col items-center space-y-8">
             <div className="relative">
-              <h2 className="text-5xl md:text-7xl font-black text-transparent bg-gradient-to-r from-teal-400 to-blue-500 bg-clip-text text-center">
+              <h2 className="text-5xl pb-4 md:text-7xl font-black text-transparent bg-gradient-to-r from-teal-400 to-blue-500 bg-clip-text text-center">
                 Projects
               </h2>
               <div className="absolute inset-0 -z-10 bg-gradient-to-r from-cyan-500/20 to-purple-500/20 blur-3xl rounded-full" />
